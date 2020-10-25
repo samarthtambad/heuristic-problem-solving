1841fbe71fd3d4af489acc042c3dd1ebf8319421
samarthtambad <tambad.samarth@gmail.com>
2020-10-25 18:50:33 -0400

    add to str in walls

--- evasion/players/wall.py
+++ evasion/players/wall.py
@@ -17,6 +17,9 @@ class HorizontalWall(Wall):
     def occupies(self, px, py) -> bool:
         return py == self.y and self.x1 <= px <= self.x2
 
+    def __str__(self):
+        return "0 {0} {1} {2}".format(self.y, self.x1, self.x2)
+
 
 class VerticalWall(Wall):
     def __init__(self, x, y1, y2):
@@ -27,6 +30,9 @@ class VerticalWall(Wall):
 
     def occupies(self, px, py) -> bool:
         return px == self.x and self.y1 <= py <= self.y2
+
+    def __str__(self):
+        return "1 {0} {1} {2}".format(self.x, self.y1, self.y2)
 
 
 class DiagonalWall(Wall):
@@ -45,3 +51,6 @@ class DiagonalWall(Wall):
 
         offset = py - self.y1
         return (self.x1 + offset == px or self.x1 + offset + 1 == px) and self.x1 <= px <= self.x2 and self.y1 <= py <= self.y2
+
+    def __str__(self):
+        return "2 {0} {1} {2} {3} {4}".format(self.x1, self.x2, self.y1, self.y2, self.build_direction)
