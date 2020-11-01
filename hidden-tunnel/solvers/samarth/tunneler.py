4370e5dac5ae73b5f5d80bc8001bb820af3d3ec8
samarthtambad <tambad.samarth@gmail.com>
2020-11-01 17:03:08 -0500

    fix

--- hidden-tunnel/solvers/samarth/tunneler.py
+++ hidden-tunnel/solvers/samarth/tunneler.py
@@ -6,26 +6,24 @@ from collections import defaultdict
 
 
 def build_tunnel(num_grid, f):
-    min_tunnel_length, max_tunnel_length = num_grid - 1, 3 * num_grid
-    tunnel_length = random.randint(min_tunnel_length, max_tunnel_length)
 
-    def dfs(vertex, target, path):
-        if len(path) > tunnel_length:
-            return []
-        if vertex == target and len(path) == tunnel_length:
-            return path
+    def dfs(depth, vertex, target, path):
+        nonlocal tunnel_verts
+        if vertex == target and depth == 0:
+            tunnel_verts = path
+            return True
+        if depth == 0:
+            return False
         for adj_vertex in graph[vertex]:
             if adj_vertex not in path:
-                path.add(adj_vertex)
-                res = dfs(adj_vertex, target, path)
-                if res:
-                    return res
-                path.remove(adj_vertex)
+                path.append(adj_vertex)
+                if dfs(depth-1, adj_vertex, target, path):
+                    return True
+                path.pop()
 
     # create graph
     graph = defaultdict(list)
-    visited = set()
-    start, target = (1, random.randint(1, num_grid)), (1, random.randint(1, num_grid))
+    start, target = (1, random.randint(1, num_grid)), (num_grid, random.randint(1, num_grid))
     for r in range(1, num_grid + 1):
         for c in range(1, num_grid + 1):
             vertex = (r, c)
@@ -34,12 +32,21 @@ def build_tunnel(num_grid, f):
                 if 1 <= new_r <= num_grid and 1 <= new_c <= num_grid:
                     graph[vertex].append((new_r, new_c))
 
-    path = dfs(start, target, set(start))
-    print(path)
-    for i in range(len(path)-1):
-        u, v = path[i], path[i+1]
+    # try to create tunnel of random length
+    min_tunnel_length, max_tunnel_length = num_grid - 1, 3 * num_grid
+    print("Start: {0}, Target: {1}".format(start, target))
+    tunnel_verts = []
+    while True:
+        tunnel_length = random.randint(min_tunnel_length, max_tunnel_length)
+        print(tunnel_length)
+        dfs(tunnel_length, start, target, [start])
+        if len(tunnel_verts) > 0:
+            break
+
+    print(tunnel_verts)
+    for i in range(len(tunnel_verts)-1):
+        u, v = tunnel_verts[i], tunnel_verts[i+1]
         f.write("{},{} {},{}\n".format(u[0], u[1], v[0], v[1]))
-        print("{},{} {},{}\n".format(u[0], u[1], v[0], v[1]))
 
 
 if __name__ == "__main__":
