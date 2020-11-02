582d5e85e6af6edaf72737e10aa04200b5904856
samarthtambad <tambad.samarth@gmail.com>
2020-11-02 08:53:37 -0500

    fix

--- hidden-tunnel/solvers/samarth/detector.py
+++ hidden-tunnel/solvers/samarth/detector.py
@@ -31,6 +31,7 @@ class Detector:
         while True:
             data = self.srv_conn.recv(DATA_SIZE).decode()
             if data:
+                print(data)
                 return json.loads(data)
 
     def run_detector(self):
